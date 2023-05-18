9baf39cbef4d5ee9c849d4cbfb0feff15e03e1c0
Fatima <132702573+Fatima-CS23@users.noreply.github.com>
2023-05-19 00:00:41 +0500

    Update print statement

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,7 +2,7 @@
 public class TeamProject {
 
 	public static void main(String[] args) {
-	System.out.println("hello world!");
+	System.out.println("Noor-e-Hira and Fatima are the team members of this project");
 
 	}
 
